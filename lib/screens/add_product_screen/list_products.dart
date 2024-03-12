3fecf740abd11c9b27b90aa411dcef624d07945a
Assassin2411 <amitjat2406@gmail.com>
2024-03-12 12:15:20 +0530

    changes in list product screen

--- lib/screens/add_product_screen/list_products.dart
+++ lib/screens/add_product_screen/list_products.dart
@@ -25,61 +25,73 @@ class _ListProductsState extends State<ListProducts> {
       ),
       body: Column(
         children: [
-          StreamBuilder<QuerySnapshot>(
-            stream: _productsStream,
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return const Text('Something went wrong');
-              }
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: _productsStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return const Text('Something went wrong');
+                }
 
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: LoadingWidget(),
-                );
-              }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: LoadingWidget(),
+                  );
+                }
 
-              return Container(
-                height: screenHeight(context) * 0.8,
-                child: ListView(
-                  children:
-                      snapshot.data!.docs.map((DocumentSnapshot document) {
-                    Map<String, dynamic> data =
-                        document.data() as Map<String, dynamic>;
-                    return Column(
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => EditScreen(
-                                      id: document.id,
-                                      name: data['name'],
-                                      price: double.parse(
-                                          data['price'].toString()),
-                                      description: data['description'],
-                                      image: data['imageUrl'],
-                                      category: data['category'],
-                                    )));
-                          },
-                          child: Container(
-                            color: const Color(0xffeeeeee),
-                            child: ListTile(
-                              leading: CachedNetworkImage(
-                                imageUrl: data['imageUrl'],
-                                height: 100,
+                return SizedBox(
+                  width: screenWidth(context),
+                  child: ListView(
+                    children:
+                        snapshot.data!.docs.map((DocumentSnapshot document) {
+                      Map<String, dynamic> data =
+                          document.data() as Map<String, dynamic>;
+                      return Column(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => EditScreen(
+                                        id: document.id,
+                                        name: data['name'],
+                                        price: double.parse(
+                                            data['price'].toString()),
+                                        description: data['description'],
+                                        image: data['imageUrl'],
+                                        category: data['category'],
+                                      )));
+                            },
+                            child: Container(
+                              color: const Color(0xffeeeeee),
+                              width: screenWidth(context),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  CachedNetworkImage(
+                                    imageUrl: data['imageUrl'],
+                                    height: 100,
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text(data['name']),
+                                      Text(data['price'].toString()),
+                                      Text(data['description']),
+                                      Text(data['category']),
+                                    ],
+                                  )
+                                ],
                               ),
-                              title: Text(data['name']),
-                              subtitle: Text(data['price'].toString()),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                      ],
-                    );
-                  }).toList(),
-                ),
-              );
-            },
+                          const SizedBox(height: 20),
+                        ],
+                      );
+                    }).toList(),
+                  ),
+                );
+              },
+            ),
           ),
           SizedBox(height: screenHeight(context) * 0.08),
         ],
